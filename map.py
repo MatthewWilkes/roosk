89b4db832b5950048c038b9a256d95dc824bdebf
Matthew Wilkes <git@matthewwilkes.name>
2012-08-10 16:48:52 +0100

    Split map utility into classes for the complex map that roosk needs, and a human friendly one

--- map.py
+++ map.py
@@ -1,8 +1,10 @@
+from PIL import Image, ImageDraw
+from cStringIO import StringIO
+
 def out():
     raw_map = open('mars.dat').readlines()
     width,height = map(int,raw_map.pop(0).split(','))
     pcols = set()
-    from PIL import Image, ImageDraw
     size = width,height
     im = Image.new('RGB', size)
     draw = ImageDraw.Draw(im)
@@ -19,14 +21,40 @@ def out():
       print ''.join(row)
     im.show()
 
+class ComplexMap(object):
+    
+    def __init__(self, filename):
+        self.image = Image.open(open(filename, "rb"))
 
-def inn():
-    raw_map = open('mars.dat', "wb")
-    from PIL import Image, ImageDraw
-    im = Image.open(open("/Users/matthewwilkes/Desktop/mars.bmp","rb"))
-    width,height = im.size
-    raw_map.write("%d,%d\n" % (width, height))
-    for y in range(height):
-        for x in range(width):
-          colour = im.getpixel((x,y))
-          raw_map.write(''.join(map(chr, colour)) + '\n')
+    def convert_to_dat(self):
+        raw_map = StringIO()
+        width,height = self.image.size
+        raw_map.write("%d,%d\n" % (width, height))
+        for y in range(height):
+            for x in range(width):
+              colour = self.image.getpixel((x,y))
+              raw_map.write(''.join(map(chr, colour)) + '\n')
+        return raw_map
+
+class SimpleMap(object):
+    
+    def __init__(self, filename):
+        self.image = Image.open(open(filename, "rb"))
+
+    def get_unique_colours(self):
+        colours = set()
+        width,height = self.image.size
+        for y in range(height):
+            for x in range(width):
+              colours.add(self.image.getpixel((x,y)))
+        return colours
+
+    def get_territories(self):
+        colours = self.get_unique_colours()
+        colours.remove((0, 0, 0)) # borders
+        colours.remove((255, 255, 255)) # water
+        colours.remove((255, 0, 0)) # bridges
+        return enumerate(colours)
+
+if __name__ == '__main__':
+    print dict(SimpleMap("/Users/matthewwilkes/Desktop/mars.bmp").get_territories())
